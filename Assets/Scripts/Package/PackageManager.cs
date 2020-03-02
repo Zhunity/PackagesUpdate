46198004a7d49a742e9cd3426c55e5b94cf11490
lihua <poinseting@163.com>
2020-03-02 15:23:47 +0800

    添加Utils

--- Assets/Scripts/Package/PackageManager.cs
+++ Assets/Scripts/Package/PackageManager.cs
@@ -23,7 +23,7 @@ public class PackageManager : MonoBehaviour
 			return;
 		}
 		Debug.Log("EditorAwake");
-		windowType = GetType(packageManagerWidowName);
+		windowType = Utils.GetType(packageManagerWidowName);
 		packageWindow = new PackageWindow(windowType);
 
 
@@ -51,39 +51,5 @@ public class PackageManager : MonoBehaviour
 			return true;
 		}
 		return false;
-	}
-
-	/// <summary>
-	/// 获取类型
-	/// </summary>
-	/// <param name="typeName"></param>
-	/// <returns></returns>
-	public static Type GetType(string typeName)
-	{
-		Type type = null;
-		Assembly[] assemblyArray = AppDomain.CurrentDomain.GetAssemblies();
-		int assemblyArrayLength = assemblyArray.Length;
-		for (int i = 0; i < assemblyArrayLength; ++i)
-		{
-			type = assemblyArray[i].GetType(typeName);
-			if (type != null)
-			{
-				return type;
-			}
-		}
-
-		for (int i = 0; (i < assemblyArrayLength); ++i)
-		{
-			Type[] typeArray = assemblyArray[i].GetTypes();
-			int typeArrayLength = typeArray.Length;
-			for (int j = 0; j < typeArrayLength; ++j)
-			{
-				if (typeArray[j].Name.Equals(typeName))
-				{
-					return typeArray[j];
-				}
-			}
-		}
-		return type;
 	}
 }
---
+++ Assets/Scripts/Reflection/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public static class Utils 
+{
+	/// <summary>
+	/// 获取类型
+	/// </summary>
+	/// <param name="typeName"></param>
+	/// <returns></returns>
+	public static Type GetType(string typeName)
+	{
+		Type type = null;
+		Assembly[] assemblyArray = AppDomain.CurrentDomain.GetAssemblies();
+		int assemblyArrayLength = assemblyArray.Length;
+		for (int i = 0; i < assemblyArrayLength; ++i)
+		{
+			type = assemblyArray[i].GetType(typeName);
+			if (type != null)
+			{
+				return type;
+			}
+		}
+
+		for (int i = 0; (i < assemblyArrayLength); ++i)
+		{
+			Type[] typeArray = assemblyArray[i].GetTypes();
+			int typeArrayLength = typeArray.Length;
+			for (int j = 0; j < typeArrayLength; ++j)
+			{
+				if (typeArray[j].Name.Equals(typeName))
+				{
+					return typeArray[j];
+				}
+			}
+		}
+		return type;
+	}
+}
