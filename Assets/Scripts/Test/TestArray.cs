45b32ada66aaede57c7106b76a9e065981357b46
lihua <poinseting@163.com>
2020-03-04 00:37:18 +0800

    单独这么测试又没什么问题，那个Item到底什么来头？

---
+++ Assets/Scripts/Test/TestArray.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestArray  : MonoBehaviour
+{
+	private void Awake()
+	{
+		MyArray array = new MyArray();
+
+		int[] a = new int[1];
+
+		Class rArray = new Class(typeof(MyArray));
+		Property Array = new Property(rArray, "Array");
+		rArray.SetInstance(array);
+		Debug.Log(Array.propertyInfo);
+
+		rArray.ShowMembersValue();
+	}
+}
+
+public class MyArray
+{
+	public int[] array = new int[] { 1, 2, 3 };
+
+	public ArrayProperty[] Array
+	{
+		get
+		{
+			return new ArrayProperty[2];
+		}
+	}
+}
+
+public class ArrayProperty
+{
+
+}
